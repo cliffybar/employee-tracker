92e7f993ac1c77d78996cb8524141164897c6d19
cliffybar <crowellclifford@utexas.edu>
2021-07-02 17:12:30 -0500

    removed apiRoutes section

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const db = require('./db/connection');
 const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -8,9 +7,6 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-// Use apiRoutes
-app.use('/api', apiRoutes);
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
